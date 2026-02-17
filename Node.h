dc28117f6718e9b7914af8f7cbe494853b8bed66
6813245_Earth <thanaporn.sog@student.mahidol.edu>
2026-02-17 23:29:13 +0700

    Refactor Node structure and remove dequeue function
    Removed unused struct member and dequeue function.

--- Node.h
+++ Node.h
@@ -2,30 +2,9 @@
 #define Node_h
 
 typedef struct node {
+    struct node* next;
     int order_number;
     int q;
-    struct node* nextPtr;
-} OrderNode;
-
-typedef OrderNode* NodePtr;
-
-#endif
-int dequeue(NodePtr* head, NodePtr* tail){
-  NodePtr t=*head;
-   if(t){
-   int value= t->data;
-   *head=t->nextPtr;
-   if(*head==NULL) *tail=NULL;
-      free(t);
-      return value;
-       
-       
-   return value;
-   }
-   printf("Empty queue");
-   return 0;
-}
-
-
+} order;
 
 #endif
