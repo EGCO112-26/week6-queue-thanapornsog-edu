9ab6ddb8c44b1b7c0225d6da70848d8586348410
6813245_Earth <thanaporn.sog@student.mahidol.edu>
2026-02-17 23:18:44 +0700

    Refactor Node structure and enqueue function
    Refactor Node structure to OrderNode and update enqueue function.

--- Node.h
+++ Node.h
@@ -1,39 +1,15 @@
-//
-//  linkedlist.h
-//  LinkedList_simple
-//
-//  Created by Mingmanas Sivaraksa on 5/2/2566 BE.
-//
 #ifndef Node_h
 #define Node_h
 
-struct node
-{
-    int data;
-    struct node *nextPtr;
-};
+typedef struct node {
+    int order_number;
+    int q;
+    struct node* nextPtr;
+} OrderNode;
 
-typedef struct node Node;
-typedef struct node* NodePtr;
+typedef OrderNode* NodePtr;
 
-
-void enqueue(NodePtr * head, NodePtr* tail, int x){
-    Node* new_node = (NodePtr) malloc(sizeof(Node));
-    if(new_node){ 
-        new_node->data = x;
-        new_node->nextPtr = NULL;
-        
-        if(*head == NULL) 
-            *head = new_node;
-        else 
-            (*tail)->nextPtr = new_node; 
-            
-        *tail = new_node;
-    }
-}
-
-
-
+#endif
 int dequeue(NodePtr* head, NodePtr* tail){
   NodePtr t=*head;
    if(t){
